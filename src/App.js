e897ce3168974eeea3ae11fc7f2d045c3fb8cfef
54padmakshi <pallavi.huilgol@gmail.com>
2024-06-04 10:09:23 +0530

    updated version

--- src/App.js
+++ src/App.js
@@ -10,7 +10,12 @@ import ContactSection from "./components/ContactSection";
 
 
 function App() {
-  
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
 
   return (
     <div className="App">
@@ -24,7 +29,13 @@ function App() {
                  <ContactSection/>
            
        </div>
-      
+       <button 
+          onClick={scrollToTop}
+         style={{textAlign:'center',position:'absolute'}}
+          className="fixed bottom-4 right-4 px-4 py-2 bg-blue-500 text-white rounded-full transition-transform duration-300 hover:scale-110"
+        >
+          Back to Top
+        </button>
     </div>
   );
 }
